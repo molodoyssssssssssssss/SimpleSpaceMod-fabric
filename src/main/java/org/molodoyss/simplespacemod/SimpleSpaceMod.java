a6560db842768facb60a0ff1ae684aba5e79632b
molodoyss <molodoj445@gmail.com>
2026-01-07 13:02:09 +0200

    updated mod to 1.1

--- src/main/java/org/molodoyss/simplespacemod/SimpleSpaceMod.java
+++ src/main/java/org/molodoyss/simplespacemod/SimpleSpaceMod.java
@@ -3,13 +3,20 @@ package org.molodoyss.simplespacemod;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageSources;
 import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
 
 public class SimpleSpaceMod implements ModInitializer {
+    public static final String MOD_ID = "ssm";
+
     @Override
     public void onInitialize() {
         DamageSources[] damageSource = new DamageSources[1];
@@ -21,9 +28,23 @@ public class SimpleSpaceMod implements ModInitializer {
         ServerTickEvents.END_WORLD_TICK.register(serverWorld -> {
             if (!serverWorld.getServer().getPlayerManager().getPlayerList().isEmpty()) {
                 for (ServerPlayerEntity player : serverWorld.getServer().getPlayerManager().getPlayerList()) {
-                    if (player.getY() >= 500 && (!player.hasStatusEffect(StatusEffects.WATER_BREATHING))) {
-                        player.damage(serverWorld, drown[0], (float)(player.getY() / 1000F));
+                    if (player.getY() >= 500) {
+                        AttributeContainer attributes = player.getAttributes();
+                        EntityAttributeInstance gravity = attributes.getCustomInstance(EntityAttributes.GRAVITY);
+                        if (gravity != null) {
+                            gravity.setBaseValue(0D);
+                        }
+                        if ((!player.hasStatusEffect(StatusEffects.WATER_BREATHING))) {
+                            player.damage(serverWorld, drown[0], (float) (player.getY() / 1000F));
+                        }
+                    } else {
+                        AttributeContainer attributes = player.getAttributes();
+                        EntityAttributeInstance gravity = attributes.getCustomInstance(EntityAttributes.GRAVITY);
+                        if (gravity != null) {
+                            gravity.setBaseValue(0.08D);
+                        }
                     }
+
                 }
             }
         });
